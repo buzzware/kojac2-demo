0bea9b057c4b3a42f74981a445d65896a40d213c
Gary McGhee <gary@buzzware.com.au>
2017-05-25 13:02:58 +0800

    added Kojac as a util and tests pass

---
+++ kojac/app/utils/Kojac.js
@@ -0,0 +1,1 @@
+export { default } from 'kojac/utils/Kojac';
---
+++ kojac/tests/unit/utils/kojac-test.js
@@ -0,0 +1,10 @@
+import Kojac from 'dummy/utils/Kojac';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | Kojac');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = new Kojac();
+  assert.ok(result);
+});
