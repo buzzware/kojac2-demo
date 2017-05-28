c9ae550878d6fca0ed295e6757ac59969571a9d6
Gary McGhee <gary@buzzware.com.au>
2017-05-28 21:39:52 +0800

    kojac tests pass

--- kojac/addon/utils/Kojac.js
+++ kojac/addon/utils/Kojac.js
@@ -1,7 +1,7 @@
 export default class {
 
-	constructor() {
-	}
+	//constructor() {
+	//}
 
 	bang() {
 		console.log('bang');
--- kojac/tests/unit/utils/kojac-test.js
+++ kojac/tests/unit/utils/kojac-test.js
@@ -1,6 +1,7 @@
 import { module, test } from 'qunit';
 import Kojac from 'kojac/utils/Kojac';
 import Immutable from 'seamless-immutable';
+import Ember from 'ember';
 
 module('Unit | Utility | Kojac');
 
@@ -23,9 +24,72 @@ test('immutable',function(assert) {
 	assert.strictEqual(obj.foo,'bar','should not change');
 });
 
+test('immutable via Ember',function(assert) {
+	var obj = Immutable({foo: "bar"});
+	assert.throws(
+		function() {
+			Ember.set(obj,'foo','me');
+		},
+		TypeError,
+		"can't mutate"
+	);
+	assert.strictEqual(obj.foo,'bar','should not change');
+});
+
+test('Ember can get',function(assert) {
+	var obj = Immutable({foo: "bar"});
+	assert.throws(
+		function() {
+			Ember.set(obj,'foo','me');
+		},
+		TypeError,
+		"can't mutate"
+	);
+	assert.strictEqual(obj.foo,'bar','should not change');
+});
+
+
 test('immutable merge',function(assert) {
 	var obj = Immutable({foo: "bar"});
 	var obj2 = Immutable.merge(obj,{foo: "me"})
 	assert.strictEqual(obj2.foo,'me','can merge');
 });
 
+test('defineProperty test',function(assert) {
+
+	function Sprite() {
+		// Constructor code ...
+	};
+
+	Object.defineProperty(Sprite.prototype, "y", {
+		set: function (val) {
+			if (typeof val !== "number" || isNaN(val)) {
+				throw "Sprite.y must be a valid number and not NaN";
+			}
+			this.__y = val;
+		},
+		get: function () {
+			return this.__y;
+		}
+	})
+
+	Sprite.prototype.y = 0;
+	Sprite.prototype.x = 0;
+
+// Instantiate
+
+	var sprite = new Sprite();
+	sprite.x = "Hello, world!"; // Passed without complaints
+
+	assert.throws(
+		function() {
+			sprite.y = sprite.somethingUndefined / 10; // Exception is thrown
+		},
+		function(e){
+			console.log("error::"+e);
+			return true;
+		},
+		"must be a number"
+	);
+	sprite.y = 10;  // no problem
+});
