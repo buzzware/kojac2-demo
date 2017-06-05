1188b8a80d3e48cd41f646e8760edbec2162cee7
Gary McGhee <gary@buzzware.com.au>
2017-06-05 08:08:11 +0800

    testem and eslint config

--- kojac/.eslintrc.js
+++ kojac/.eslintrc.js
@@ -9,6 +9,17 @@ module.exports = {
     browser: true
   },
   rules: {
-	  "no-console":0
+	  "no-console":0,
+	  "no-unused-vars":0,
+		"quotes": 0,
+    "indent": 0,
+	  "no-unreachable": 0,
+	  "no-mixed-spaces-and-tabs": 0,
+    "func-names": 0,
+    "no-use-before-define": [2, "nofunc"],
+    "prefer-arrow-callback": 0,
+    "prefer-rest-params": 0,
+    "import/no-unresolved": 0,
+    "no-underscore-dangle": 0
   }
 };
