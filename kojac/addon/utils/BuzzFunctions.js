4d5258f0b1d20139d562406bb0384918ed2806c2
Gary McGhee <gary@buzzware.com.au>
2017-06-05 08:07:52 +0800

    work on modules

--- kojac/addon/utils/BuzzFunctions.js
+++ kojac/addon/utils/BuzzFunctions.js
@@ -1,17 +1,16 @@
 import _ from 'lodash';
-var bf = BuzzFunctions = {
-};
+var bf = {};
 
 //BEGIN copied from underscore
-var ArrayProto = Array.prototype, ObjProto = Object.prototype, FuncProto = Function.prototype;
- // Create quick reference variables for speed access to core prototypes.
- var slice            = ArrayProto.slice;
+var ArrayProto = Array.prototype; //, ObjProto = Object.prototype, FuncProto = Function.prototype;
+// Create quick reference variables for speed access to core prototypes.
+var slice = ArrayProto.slice;
 //END copied from underscore
 
-bf.stringify = function(aObject) {
+bf.stringify = function (aObject) {
 	if (_.isString(aObject))
 		return aObject;
-	if (aObject===null || aObject===undefined)
+	if (aObject === null || aObject === undefined)
 		return '';
 	if (aObject.toString)
 		return aObject.toString();
@@ -47,31 +46,33 @@ bf.stringify = function(aObject) {
 //              }
 //       }
 
-bf.concat = function(aArray,aAnotherArray) {
+bf.concat = function (aArray, aAnotherArray) {
 	var result = [];
 	result.push.apply(result, aArray);
 	result.push.apply(result, aAnotherArray);
 	return result;
 };
 
-bf.nameValueString = function(aObject) {
-	return _.map(_.keys(aObject),function(k) { return k+'="'+bf.stringify(aObject[k])+'"'}).join(' ');
+bf.nameValueString = function (aObject) {
+	return _.map(_.keys(aObject), function (k) {
+		return k + '="' + bf.stringify(aObject[k]) + '"'
+	}).join(' ');
 };
 
-bf.classEnsure = function(aOrig,aNew) {
+bf.classEnsure = function (aOrig, aNew) {
 	if (!aOrig)
 		return aNew;
 	if (!aNew)
 		return aOrig;
-	return _.union(aOrig.split(' '),aNew.split(' '));
+	return _.union(aOrig.split(' '), aNew.split(' '));
 };
 
-_.getPath =	function(aObject,aPath,aDefault) {
-	if ((typeof aObject)=='string') {   // allow aObject to be left out and assume this
-		if (arguments.length==1) {
+_.getPath = function (aObject, aPath, aDefault) {
+	if ((typeof aObject) == 'string') {   // allow aObject to be left out and assume this
+		if (arguments.length == 1) {
 			aPath = aObject;
 			aObject = window;
-		} else if (arguments.length==2) {
+		} else if (arguments.length == 2) {
 			aDefault = aPath;
 			aPath = aObject;
 			aObject = window;
@@ -79,13 +80,13 @@ _.getPath =	function(aObject,aPath,aDefault) {
 	}
 	var nodes = aPath.split('.');
 	var curr = aObject;
-	for (var i=0;i<nodes.length;i++) {
+	for (var i = 0; i < nodes.length; i++) {
 		var name = nodes[i];
-		if ((curr===undefined)||(curr===null))
+		if ((curr === undefined) || (curr === null))
 			return aDefault;
 		if ((name in curr) || curr[name]) {
 			var val = curr[name];
-			if ((typeof val)=='function')
+			if ((typeof val) == 'function')
 				curr = val.call(curr);
 			else
 				curr = val;
@@ -96,12 +97,12 @@ _.getPath =	function(aObject,aPath,aDefault) {
 	return curr;
 }
 
-bf.moveKeys = function(aDest,aSource,aKeys) {
+bf.moveKeys = function (aDest, aSource, aKeys) {
 	if (!aSource)
 		return aDest;
 	if (!aKeys)
 		aKeys = _.keys(aSource);
-	for (var i=0;i<aKeys.length;i++) {
+	for (var i = 0; i < aKeys.length; i++) {
 		var k = aKeys[i];
 		if (!(k in aSource))
 			continue;
@@ -111,124 +112,124 @@ bf.moveKeys = function(aDest,aSource,aKeys) {
 	return aDest;
 };
 
-bf.removeKey = function(aObject,aKey) {
+bf.removeKey = function (aObject, aKey) {
 	var result = aObject[aKey];
 	delete aObject[aKey];
 	return result;
 };
 
-bf.hasMatchingProperties = function(aObject,aCriteria) {
+bf.hasMatchingProperties = function (aObject, aCriteria) {
 	for (var p in aCriteria) {
-		if (!(p in aObject) || (aObject[p]!==aCriteria[p]))
+		if (!(p in aObject) || (aObject[p] !== aCriteria[p]))
 			return false;
 	}
 	return true;
 };
 
 // find one item matching object properties
-bf.findByCriteria = function(aArray,aCriteria) {
-	return _.find(aArray,function(obj){
-		return bf.hasMatchingProperties(obj,aCriteria);
+bf.findByCriteria = function (aArray, aCriteria) {
+	return _.find(aArray, function (obj) {
+		return bf.hasMatchingProperties(obj, aCriteria);
 	});
 };
 
 // find one item matching object properties
-bf.filterByCriteria = function(aArray,aCriteria) {
-	return _.filter(aArray,function(obj){
-		return bf.hasMatchingProperties(obj,aCriteria);
+bf.filterByCriteria = function (aArray, aCriteria) {
+	return _.filter(aArray, function (obj) {
+		return bf.hasMatchingProperties(obj, aCriteria);
 	});
 };
 
-bf.isObjectStrict = function(aSomething) {
-	return Object.prototype.toString.call(aSomething)==='[object Object]';
+bf.isObjectStrict = function (aSomething) {
+	return Object.prototype.toString.call(aSomething) === '[object Object]';
 }
 
 // create an object using key,value arguments eg. createObject('a',2) returns {a: 2}
-bf.createObject = function() {
+bf.createObject = function () {
 	var result = {};
 	result[arguments[0]] = arguments[1];
 	return result;
 }
 
-bf.endsWith = function(aString, aSuffix) {
+bf.endsWith = function (aString, aSuffix) {
 	var i = aString.lastIndexOf(aSuffix);
-	return (i>=0 && i===aString.length-aSuffix.length);
+	return (i >= 0 && i === aString.length - aSuffix.length);
 }
 
-bf.beginsWith = function(aString, aPrefix) {
+bf.beginsWith = function (aString, aPrefix) {
 	var i = aString.indexOf(aPrefix);
-	return (i==0);
+	return (i == 0);
 }
 
-bf.chop = function(aString, aSuffix) {
+bf.chop = function (aString, aSuffix) {
 	var i = aString.lastIndexOf(aSuffix);
-	return (i===aString.length-aSuffix.length) ? aString.substring(0,i) : aString;
+	return (i === aString.length - aSuffix.length) ? aString.substring(0, i) : aString;
 }
 
-bf.bite = function(aString, aPrefix) {
+bf.bite = function (aString, aPrefix) {
 	var i = aString.indexOf(aPrefix);
-	return (i===0) ? aString.substring(aPrefix.length) : aString;
+	return (i === 0) ? aString.substring(aPrefix.length) : aString;
 }
 
 // undefined null function arguments string number date regexp object
-bf.typeOf = function(aSomething) {
-	if (aSomething===undefined)
+bf.typeOf = function (aSomething) {
+	if (aSomething === undefined)
 		return 'undefined';
-	if (aSomething===null)
+	if (aSomething === null)
 		return 'null';
 
 	var result = Object.prototype.toString.call(aSomething);
-	result = bf.bite(result,'[object ');
-	result = bf.chop(result,']');
+	result = bf.bite(result, '[object ');
+	result = bf.chop(result, ']');
 	return result.toLowerCase();
 }
 
-	/**
-	 * Clone properties that are objects or arrays, otherwise if aSource and aDestination are different, properties will be copied
-	 * @param aDestination
-	 * @param aSource  (optional, defaults to aSource)
-	 * @return aDestination
-	 */
+/**
+ * Clone properties that are objects or arrays, otherwise if aSource and aDestination are different, properties will be copied
+ * @param aDestination
+ * @param aSource  (optional, defaults to aSource)
+ * @return aDestination
+ */
 bf.cloneComplexValues = function (aDestination, aSource) {
 	if (!aSource)
 		aSource = aDestination;
 	for (var p in aSource) {
 		var t = bf.typeOf(aSource[p]);
-		if (t==='array' || t==='object')
+		if (t === 'array' || t === 'object')
 			aDestination[p] = bf.clone(aSource[p]);
-		else if (aDestination!==aSource)
+		else if (aDestination !== aSource)
 			aDestination[p] = aSource[p];
 	}
 	return aDestination;
 }
 
-	// http://justtalkaboutweb.com/2008/01/06/javascript-object-extension/
+// http://justtalkaboutweb.com/2008/01/06/javascript-object-extension/
 
 //bf.originalClone = _.originalClone || _.clone;
 // Create a copy (shallow or deep) of an object from https://github.com/cederberg/underscore/commits/feature-clone
-bf.clone = function(obj, deep, ignoreFns) {
+bf.clone = function (obj, deep, ignoreFns) {
 	if (!deep && !ignoreFns)
 		return _.clone(obj);
-  if (!_.isObject(obj) || _.isFunction(obj)) return obj;
-  if (_.isDate(obj)) return new Date(obj.getTime());
-  if (_.isRegExp(obj)) return new RegExp(obj.source, obj.toString().replace(/.*\//, ""));
-  var isArr = (_.isArray(obj) || _.isArguments(obj));
-  if (deep) {
-    var func = function (memo, value, key) {
-      if (!ignoreFns || !_.isFunction(value)) {
-      if (isArr)
-        memo.push(bf.clone(value, true));
-      else
-        memo[key] = bf.clone(value, true);
-      }
-      return memo;
-    };
-    return _.reduce(obj, func, isArr ? [] : {});
-  } else {
-	  var result;
+	if (!_.isObject(obj) || _.isFunction(obj)) return obj;
+	if (_.isDate(obj)) return new Date(obj.getTime());
+	if (_.isRegExp(obj)) return new RegExp(obj.source, obj.toString().replace(/.*\//, ''));
+	var isArr = (_.isArray(obj) || _.isArguments(obj));
+	if (deep) {
+		var func = function (memo, value, key) {
+			if (!ignoreFns || !_.isFunction(value)) {
+				if (isArr)
+					memo.push(bf.clone(value, true));
+				else
+					memo[key] = bf.clone(value, true);
+			}
+			return memo;
+		};
+		return _.reduce(obj, func, isArr ? [] : {});
+	} else {
+		var result;
 		if (isArr)
 			result = slice.call(obj);
-	  else {
+		else {
 			result = {};
 			var v;
 			for (var p in obj) {
@@ -236,10 +237,10 @@ bf.clone = function(obj, deep, ignoreFns) {
 				if (ignoreFns && _.isFunction(v))
 					continue;
 				result[p] = v;
-      }
+			}
 		}
 		return result;
-  }
+	}
 };
 
 bf.randomString = function (aLength, aCharSet) {
@@ -256,75 +257,75 @@ bf.randomString = function (aLength, aCharSet) {
 	return result.join('');
 };
 
-bf.extract = function(aString,aRegex) {
+bf.extract = function (aString, aRegex) {
 	var matches = aRegex.exec(aString);
 	return matches && matches.length ? matches[0] : null;
 };
 
 bf.NUMBER_REGEX = /[-+]?([0-9]*\.[0-9]+|[0-9]+)/;
-bf.extractNumber = function(aString,aDefault) {
-	return bf.toNumber(_.extract(aString,_.NUMBER_REGEX),aDefault);
+bf.extractNumber = function (aString, aDefault) {
+	return bf.toNumber(_.extract(aString, _.NUMBER_REGEX), aDefault);
 };
 
-bf.formatNumber = function(aValue,aDecimals) {
-	if (aValue===null)
+bf.formatNumber = function (aValue, aDecimals) {
+	if (aValue === null)
 		return '';
-	if (!aDecimals && aDecimals!==0)
+	if (!aDecimals && aDecimals !== 0)
 		aDecimals = 2;
 	return aValue.toFixed(aDecimals);
 };
 
-bf.groupDigits = function(x) {
-	var parts = x.toString().split(".");
-	parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	return parts.join(".");
+bf.groupDigits = function (x) {
+	var parts = x.toString().split('.');
+	parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+	return parts.join('.');
 };
 
-bf.sum = function() {
+bf.sum = function () {
 	var result = NaN;
-	for (var i=0;i<arguments.length;i++) {
+	for (var i = 0; i < arguments.length; i++) {
 		var v = arguments[i];
 		if (!bf.isNumber(v))
 			return NaN;
-		if (i==0)
+		if (i == 0)
 			result = 0;
 		result += v;
 	}
 	return result;
 };
 
-bf.formatCurrency = function(aNumber,aPrecision,aSymbol) {
+bf.formatCurrency = function (aNumber, aPrecision, aSymbol) {
 	if (!aPrecision)
 		aPrecision = 0;
 	if (!aSymbol && !_.isString(aSymbol))
 		aSymbol = '$';
-	return _.isFinite(aNumber) ? aSymbol+bf.groupDigits(bf.formatNumber(aNumber,aPrecision)) : '';
+	return _.isFinite(aNumber) ? aSymbol + bf.groupDigits(bf.formatNumber(aNumber, aPrecision)) : '';
 };
 
-bf.formatPercent = function(aNumber,aPrecision,aSymbol) {
+bf.formatPercent = function (aNumber, aPrecision, aSymbol) {
 	if (!aPrecision)
 		aPrecision = 0;
 	if (!aSymbol)
 		aSymbol = '%';
-	return _.isFinite(aNumber) ? bf.groupDigits(bf.formatNumber(aNumber*100.0,aPrecision))+aSymbol : '';
+	return _.isFinite(aNumber) ? bf.groupDigits(bf.formatNumber(aNumber * 100.0, aPrecision)) + aSymbol : '';
 };
 
-bf.toNull = function(aValue,aDefault) {
-	if (arguments.length==1)
+bf.toNull = function (aValue, aDefault) {
+	if (arguments.length == 1)
 		aDefault = null;
-	return ((aValue===null) || (aValue===undefined) || (aValue===[]) || (aValue===0) || (aValue==={}) || _.isNaN(aValue)) ? aDefault : aValue;
-}
+	return ((aValue === null) || (aValue === undefined) || (aValue === []) || (aValue === 0) || (aValue === {}) || _.isNaN(aValue)) ? aDefault : aValue;
+};
 
-bf.toFinite = function(aValue,aDefault) {
-	return bf.toNumber(aValue,aDefault);
-}
+bf.toFinite = function (aValue, aDefault) {
+	return bf.toNumber(aValue, aDefault);
+};
 
-bf.toInt = function(aValue,aDefault) {
-	if (arguments.length==1)
+bf.toInt = function (aValue, aDefault) {
+	if (arguments.length == 1)
 		aDefault = null;
 	var t = bf.typeOf(aValue);
 	var result;
-	switch(t) {
+	switch (t) {
 		case 'undefined':
 		case 'null':
 		case 'array':
@@ -353,12 +354,12 @@ bf.toInt = function(aValue,aDefault) {
 	return result;
 };
 
-bf.toNumber = function(aValue,aDefault) {
-	if (arguments.length==1)
+bf.toNumber = function (aValue, aDefault) {
+	if (arguments.length == 1)
 		aDefault = null;
 	var t = bf.typeOf(aValue);
 	var result;
-	switch(t) {
+	switch (t) {
 		case 'undefined':
 		case 'null':
 		case 'array':
@@ -387,12 +388,12 @@ bf.toNumber = function(aValue,aDefault) {
 	return result;
 };
 
-bf.toBoolean = function(aValue,aDefault) {
-	if (arguments.length==1)
+bf.toBoolean = function (aValue, aDefault) {
+	if (arguments.length == 1)
 		aDefault = false;
-	if (aValue===true || aValue===false)
+	if (aValue === true || aValue === false)
 		return aValue;
-	if (aValue===0)
+	if (aValue === 0)
 		return false;
 	if (_.isString(aValue)) {
 		var t = Number(aValue);
@@ -400,9 +401,9 @@ bf.toBoolean = function(aValue,aDefault) {
 			return !!t;
 		} else {
 			t = aValue.toLowerCase();
-			if (t==="true" || t==="yes" || t==="on")
+			if (t === 'true' || t === 'yes' || t === 'on')
 				return true;
-			if (t==="false" || t==="no" || t==="off")
+			if (t === 'false' || t === 'no' || t === 'off')
 				return false;
 		}
 	} else if (_.isNumber(aValue)) {
@@ -417,20 +418,20 @@ bf.toBoolean = function(aValue,aDefault) {
 // => [{id: "cash", name:"Cash"}, {id: "consumer_mortgage",name: "Consumer Mortgage"}]
 // _.expand_options(["cash","consumer_mortgage"])
 // => [{id: "cash", name:"cash"}, {id: "consumer_mortgage",name: "consumer_mortgage"}]
-bf.expand_options = function(aObject,aIdField,aNameField) {
+bf.expand_options = function (aObject, aIdField, aNameField) {
 	if (!aIdField)
 		aIdField = 'id';
 	if (!aNameField)
 		aNameField = 'name';
 	if (_.isArray(aObject)) {
-		return _.map(aObject,function(v){
+		return _.map(aObject, function (v) {
 			var result = {};
 			result[aIdField] = v;
 			result[aNameField] = String(v);
 			return result;
 		});
 	} else {
-		return _.map(aObject,function(v,k){
+		return _.map(aObject, function (v, k) {
 			var result = {};
 			result[aIdField] = k;
 			result[aNameField] = v;
@@ -440,36 +441,36 @@ bf.expand_options = function(aObject,aIdField,aNameField) {
 };
 
 // returns array of keys on object beginning with aPrefix
-bf.keysWithPrefix = function(aObject,aPrefix) {
+bf.keysWithPrefix = function (aObject, aPrefix) {
 	var results = [];
-  var keys = _.keys(aObject);
-  for (var i=0;i<keys.length;i++) {
-	  var k = keys[i];
-	  if (!bf.beginsWith(k,aPrefix))
-	    continue;
-	  results.push(k);
-  }
+	var keys = _.keys(aObject);
+	for (var i = 0; i < keys.length; i++) {
+		var k = keys[i];
+		if (!bf.beginsWith(k, aPrefix))
+			continue;
+		results.push(k);
+	}
 	return results;
 };
 
 // returns copy of object containing only properties beginning with aPrefix
-bf.pickWithPrefix = function(aObject,aPrefix) {
+bf.pickWithPrefix = function (aObject, aPrefix) {
 	var result = {};
-	_.each(aObject,function(v, k){
-		if (!bf.beginsWith(k,aPrefix))
+	_.each(aObject, function (v, k) {
+		if (!bf.beginsWith(k, aPrefix))
 			return;
-    result[k] = v;
+		result[k] = v;
 	});
 	return result;
 };
 
-bf.round = function(aNumber,aDecimals) {
+bf.round = function (aNumber, aDecimals) {
 	var mult = Math.pow(10, aDecimals);
-	return Math.round(aNumber*mult)/mult;
+	return Math.round(aNumber * mult) / mult;
 };
 
 // copies properties, excluding functions
-bf.copyProperties = function(aDest,aSource,aProperties,aExclude) {
+bf.copyProperties = function (aDest, aSource, aProperties, aExclude) {
 	var p;
 	var v;
 	if (!aDest)
@@ -479,12 +480,12 @@ bf.copyProperties = function(aDest,aSource,aProperties,aExclude) {
 	if (aExclude && !_.isArray(aExclude))
 		aExclude = [aExclude];
 	if (aProperties) {
-		for (var i=0;i<aProperties.length;i++) {
+		for (var i = 0; i < aProperties.length; i++) {
 			p = aProperties[i];
 			v = aSource[p];
 			if (_.isFunction(v))
 				continue;
-			if (aExclude && aExclude.indexOf(p)>=0)
+			if (aExclude && aExclude.indexOf(p) >= 0)
 				continue;
 			if (p in aSource) aDest[p] = v;
 		}
@@ -493,7 +494,7 @@ bf.copyProperties = function(aDest,aSource,aProperties,aExclude) {
 			v = aSource[p];
 			if (_.isFunction(v))
 				continue;
-			if (aExclude && aExclude.indexOf(p)>=0)
+			if (aExclude && aExclude.indexOf(p) >= 0)
 				continue;
 			if (p in aSource) aDest[p] = v;
 		}
@@ -507,41 +508,41 @@ var formatRegexes = {};
 // Usage: _.format("{0} i{2}a night{1}", "This", "mare", "s ");
 // Could support _.format("{number} {street}, {suburb}", {number: 27, street: "Constant St", suburb: "Highgate"}); - might already
 // see http://stackoverflow.com/questions/2534803/string-format-in-javascript/2534870#2534870
-bf.format = function(aFormat, aValues) {
-	var src,v;
+bf.format = function (aFormat, aValues) {
+	var src, v;
 	if (_.isObject(aValues)) {
-		for (p in aValues)
-			aFormat = aFormat.replace(formatRegexes[p] || (formatRegexes[p] = RegExp("\\{" + p + "\\}", "gm")), aValues[p]);
+		for (var p in aValues)
+			aFormat = aFormat.replace(formatRegexes[p] || (formatRegexes[p] = RegExp('\\{' + p + '\\}', 'gm')), aValues[p]);
 	} else {
 		for (var args = arguments, i = args.length; --i;)
-	    aFormat = aFormat.replace(formatRegexes[i - 1] || (formatRegexes[i - 1] = RegExp("\\{" + (i - 1) + "\\}", "gm")), args[i]);
-}
+			aFormat = aFormat.replace(formatRegexes[i - 1] || (formatRegexes[i - 1] = RegExp('\\{' + (i - 1) + '\\}', 'gm')), args[i]);
+	}
 	return aFormat;
 };
 
-bf.randomInt = function(aValues) {
-	return Math.floor(Math.random()*aValues);
+bf.randomInt = function (aValues) {
+	return Math.floor(Math.random() * aValues);
 };
 
-bf.randomIntRange = function(aMin,aMax) {
-	return aMin + Math.floor(Math.random()*(aMax-aMin+1));
+bf.randomIntRange = function (aMin, aMax) {
+	return aMin + Math.floor(Math.random() * (aMax - aMin + 1));
 };
 
-bf.compactObject = function(aObject) {
+bf.compactObject = function (aObject) {
 	var result = {}
-	_.each(aObject,function(v,k){
-		if ((v===null) || (v===undefined))
+	_.each(aObject, function (v, k) {
+		if ((v === null) || (v === undefined))
 			return;
 		result[k] = v;
 	});
 	return result;
 };
 
-bf.humanize = function(property) {
-  return property.replace(/_/g, ' ')
-    .replace(/(\w+)/g, function(match) {
-      return match.charAt(0).toUpperCase() + match.slice(1);
-    });
+bf.humanize = function (property) {
+	return property.replace(/_/g, ' ')
+		.replace(/(\w+)/g, function (match) {
+			return match.charAt(0).toUpperCase() + match.slice(1);
+		});
 };
 
 export default bf;
---
+++ kojac/addon/utils/KojacModel.js
@@ -0,0 +1,266 @@
+import _ from 'lodash';
+import bf from './BuzzFunctions';
+//import Can from 'can-namespace';
+//import plugin from 'can-construct';
+//import can from 'can'
+//import can from 'npm:can';
+
+import KojacObject from './KojacObject';
+
+/**
+ * Extends Kojac.Object to support typed attributes
+ * @class Kojac.Model
+ * @extends Kojac.Object
+ **/
+var KojacModel = KojacObject.extend({
+	/**
+	 * This method is called when inheriting a new model from Kojac.Model, and allows attributes to be defined as
+	 *   name: Class (default value is null)
+	 * or
+	 *   name: default value (class is inferred)
+	 * or
+	 *   name: [Class,default value]
+	 * @param prop Hash of attributes defined as above
+	 * @return Hash of attributes in expected name:value format
+	 */
+	setup: function(prop) {
+		this.__attributes = (this._superClass && this._superClass.__attributes && bf.clone(this._superClass.__attributes)) || {};
+		//this.__defaults = (constructor.__defaults && _.clone(constructor.__defaults)) || {};
+		for (var p in prop) {
+			if (['__defaults','__attributes'].indexOf(p)>=0)
+				continue;
+			var propValue = prop[p];
+			if (_.isArray(propValue) && (propValue.length===2) && (KojacModel.FieldTypes.indexOf(propValue[0])>=0)) {  // in form property: [Type, Default Value]
+				this.__attributes[p] = propValue[0];
+				prop[p] = propValue[1];
+			} else if (KojacModel.FieldTypes.indexOf(propValue) >= 0) {   // field type
+				prop[p] = null;
+				this.__attributes[p] = propValue;
+				//this.__defaults[p] = null;
+			} else if (_.isFunction(propValue)) {
+				continue;
+			} else {        // default value
+				var i = KojacModel.FieldTypes.indexOf(KojacModel.getPropertyValueType(propValue));
+				if (i >= 0) {
+					this.__attributes[p] = KojacModel.FieldTypes[i];
+				} else {
+					this.__attributes[p] = null;
+				}
+				//this.__defaults[p] = v;
+			}
+		}
+		return prop;
+	},
+	/**
+	 * The base constructor for Kojac.Model. When creating an instance of a model, an optional hash aValues provides attribute values that override the default values
+	 * @param aValues
+	 * @constructor
+	 */
+	init: function(aValues){
+		// we don't use base init here
+		if (!aValues)
+			return;
+		for (var p in aValues) {
+			if (this.isAttribute(p)) {
+				this.attr(p,aValues[p]);
+			} else {
+				this[p] = aValues[p];
+			}
+		}
+	},
+
+	/**
+	 * Determines whether the given name is defined as an attribute in the model definition. Attributes are properties with an additional class and default value
+	 * @param aName
+	 * @return {Boolean}
+	 */
+	isAttribute: function(aName) {
+		return this.constructor.__attributes && (aName in this.constructor.__attributes);
+	},
+
+	/**
+	 * Used various ways to access the attributes of a model instance.
+	 * 1. attr() returns an object of all attributes and their values
+	 * 2. attr(<name>) returns the value of a given attribute
+	 * 3. attr(<name>,<value>) sets an attribute to the given value after converting it to the attribute's class
+	 * 4. attr({Object}) sets each of the given attributes to the given value after converting to the attribute's class
+	 * @param aName
+	 * @param aValue
+	 * @return {*}
+	 */
+	attr: function(aName,aValue) {
+		if (aName===undefined) {  // read all attributes
+			return _.pick(this, _.keys(this.constructor.__attributes));
+		} else if (aValue===undefined) {
+			if (_.isObject(aName)) {  // write all given attributes
+				aValue = aName;
+				aName = undefined;
+				if (!this.constructor.__attributes)
+					return {};
+				_.extend(this,_.pick(aValue,_.keys(this.constructor.__attributes)))
+			} else {                  // read single attribute
+				return (_.has(this.constructor.__attributes,aName) && this[aName]) || undefined;
+			}
+		} else {  // write single attribute
+			var t = this.constructor.__attributes[aName];
+			if (t)
+				aValue = KojacModel.interpretValueAsType(aValue,t);
+			return (this[aName]=aValue);
+		}
+	}
+});
+
+KojacModel.Int = {name: 'Int', toString: function() {return 'Int';}};    // represents a virtual integer type
+KojacModel.Null = {name: 'Null', toString: function() {return 'Null';}}; // represents a virtual Null type
+KojacModel.FieldTypes = [KojacModel.Null,KojacModel.Int,Number,String,Boolean,Date,Array,Object];  // all possible types for fields in Kojac.Model
+KojacModel.FieldTypeStrings = ['Null','Int','Number','String','Boolean','Date','Array','Object'];  // String names for FieldTypes
+KojacModel.SimpleTypes = [KojacModel.Null,KojacModel.Int,Number,String,Boolean,Date];  // simple field types in Kojac.Model ie. Object and Array are considered complex
+
+/*
+ * Function used to determine the data type class of the given value
+ * @param {*} aValue
+ * @return {Class} eg. see Kojac.FieldTypes
+ */
+KojacModel.getPropertyValueType = function(aValue) {
+	var t = _.typeOf(aValue);
+	var result;
+	switch(t) {
+		case 'number':   // determine number or int
+			result = (Math.floor(aValue) === aValue) ? KojacModel.Int : Number;
+			break;
+		default:
+		case 'undefined':
+		case 'null':
+			result = KojacModel.Null;
+			break;
+		case 'string':
+			result = String;
+			break;
+		case 'boolean':
+			result = Boolean;
+			break;
+		case 'array':
+			result = Array;
+			break;
+		case 'object':
+			result = Object;
+			break;
+		case 'date':
+			result = Date;
+			break;
+		case 'function':
+		case 'class':
+		case 'instance':
+		case 'error':
+			result = null;
+			break;
+	}
+	return result;
+};
+
+
+/*
+ * Function used to interpret aValue as the given aDestType which is one of the supported data type classes
+ * @param {*} aValue any value
+ * @param {Class} aDestType Class used to interpret aValue
+ * @return {*} aValue interpreted as destination type
+ */
+KojacModel.interpretValueAsType = function(aValue, aDestType) {
+	var sourceType = KojacModel.getPropertyValueType(aValue);
+	if (aDestType===sourceType)
+		return aValue;
+	switch (aDestType) {
+		case KojacModel.Null:
+			return aValue;
+		case String:
+
+			switch(sourceType) {
+				case KojacModel.Int:
+				case Number:
+				case Boolean:
+					return aValue.toString();
+					break;
+				case Date:
+					var d = Date.parse(aValue);
+					return d.toISOString();
+				default:
+				case KojacModel.Null:
+					return null;
+					break;
+			}
+
+			break;
+		case Boolean:
+			return _.toBoolean(aValue,null);
+			break;
+
+		case Number:
+
+			switch(sourceType) {
+				case KojacModel.Null:
+				default:
+					return null;
+					break;
+				case Boolean:
+					return aValue ? 1 : 0;
+					break;
+				case KojacModel.Int:
+					return aValue;
+					break;
+				case String:
+					if (aValue.trim()=='')
+						return null;
+					var n = Number(aValue);
+					return isFinite(n) ? n : null;
+					break;
+			}
+			break;
+
+		case KojacModel.Int:
+
+			switch(sourceType) {
+				case KojacModel.Null:
+				default:
+					return null;
+					break;
+				case Boolean:
+					return aValue ? 1 : 0;
+					break;
+				case Number:
+					return isFinite(aValue) ? Math.round(aValue) : null;
+					break;
+				case String:
+					if (aValue.trim()=='')
+						return null;
+					var nv = Number(aValue);
+					return isFinite(nv) ? Math.round(nv) : null;
+					break;
+			}
+
+			break;
+		case Date:
+			switch(sourceType) {
+				case String:
+					return Date.parse(aValue);
+					break;
+				case Number:
+					return new Date(aValue);
+					break;
+				case KojacModel.Null:
+				default:
+					return null;
+					break;
+			}
+			break;
+		case Object:
+			return null;
+			break;
+		case Array:
+			return null;
+			break;
+	}
+	return null;
+};
+
+
+export default KojacModel;
